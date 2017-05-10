d0e1ebab86d27b7fd51bf2397ed7a8f91df0c17a
vheimpel <vheimpel@gmail.com>
2017-05-10 18:35:06 +0000

    final

--- w2d2/tiny-app/express_server.js
+++ w2d2/tiny-app/express_server.js
@@ -7,9 +7,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
 
+var shortURL = "";
+var longURL = "";
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 function generateRandomString() {
@@ -24,6 +27,10 @@ app.get("/", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -33,31 +40,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
 app.get("/u/:shortURL", (req, res) => {
-  // let longURL = ...
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(urlDatabase[req.params.shortURL])
   res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { newShortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+  console.log(req)
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+  // console.log(req.body.longURL);  // debug statement to see POST parameters
+  let newShortUrl = generateRandomString();
+  urlDatabase[newShortUrl] = req.body.longURL;
+  res.redirect("/urls/" + newShortUrl);         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-generateRandomString(0, 36)
